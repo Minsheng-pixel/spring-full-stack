8eee68fa21a6cc11f69b7f97b5cd39d08e93cb4a
Minsheng-pixel <mc7280@nyu.edu>
2022-05-20 10:07:49 -0400

    CRM website

---
+++ src/main/java/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package springdemo.dao;
+
+import java.util.List;
+
+import springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomers(int theId);
+
+	public void deleteCustomers(int theId);
+
+	public List<Customer> searchCustomers(String theSearchName);
+}
---
+++ src/main/java/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,60 @@
+package springdemo.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import springdemo.dao.CustomerDAO;
+import springdemo.entity.Customer;
+
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		return customerDAO.getCustomers();
+		
+		
+	}
+	
+	@Override
+	@Transactional
+	public void saveCustomer(Customer theCustomer) {
+		customerDAO.saveCustomer(theCustomer);
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@Transactional
+	public Customer getCustomers(int theId) {
+		// TODO Auto-generated method stub
+		return customerDAO.getCustomers(theId);
+	}
+	@Transactional
+	@Override
+	public void deleteCustomer(int theId) {
+		// TODO Auto-generated method stub
+		customerDAO.deleteCustomers(theId);
+		
+	}
+
+	@Override
+	@Transactional
+	public List<Customer> searchCustomers(String theSearchName) {
+		// TODO Auto-generated method stub
+		return customerDAO.searchCustomers(theSearchName);
+	}
+
+}
